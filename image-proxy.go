99262fb6d4ca00371262a0db4fd83d99e13a6ab4
Jaroslav Hranička <me@hranicka.cz>
2019-03-17 11:13:25 +0100

    add resize support

--- image-proxy.go
+++ image-proxy.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image/color"
 	"log"
 	"net/http"
 	"net/url"
@@ -15,7 +16,9 @@ import (
 var (
 	listenAddr  = flag.String("listen", "localhost:8080", "Listening address")
 	timeout     = flag.String("timeout", "10", "HTTP timeout in seconds")
-	sourceParam = flag.String("source", "url", "Source URL query string parameter")
+	paramSource = flag.String("param-source", "url", "Source URL query string parameter")
+	paramWidth  = flag.String("param-width", "w", "Width query string parameter")
+	paramHeight = flag.String("param-height", "h", "Height query string parameter")
 )
 
 func main() {
@@ -25,15 +28,24 @@ func main() {
 	client := &http.Client{Timeout: time.Duration(timeoutSec) * time.Second}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		src := r.URL.Query().Get(*sourceParam)
-		if src == "" {
+		// parse and validate parameters
+		srcURL := r.URL.Query().Get(*paramSource)
+		if srcURL == "" {
 			http.Error(w, "source url parameter is required", http.StatusBadRequest)
 			return
 		}
-		log.Println(src)
+
+		width, _ := strconv.Atoi(r.URL.Query().Get(*paramWidth))
+		height, _ := strconv.Atoi(r.URL.Query().Get(*paramHeight))
+		if width < 0 || height < 0 {
+			http.Error(w, "width or height parameter is invalid", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("src %s, size %dx%d\n", srcURL, width, height)
 
 		// parse and validate source url
-		u, err := url.Parse(src)
+		u, err := url.Parse(srcURL)
 		if err != nil {
 			http.Error(w, "cannot parse url: "+err.Error(), http.StatusBadRequest)
 			return
@@ -44,7 +56,7 @@ func main() {
 		}
 
 		// load source file
-		resp, err := client.Get(src)
+		resp, err := client.Get(srcURL)
 		if err != nil {
 			http.Error(w, "download failed: "+err.Error(), http.StatusBadRequest)
 			return
@@ -57,14 +69,21 @@ func main() {
 		}
 
 		// decode source image
-		img, err := imaging.Decode(resp.Body)
+		srcImg, err := imaging.Decode(resp.Body)
 		if err != nil {
 			http.Error(w, "failed to open image: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// resize if needed
+		dstImg := srcImg
+		if width > 0 && height > 0 {
+			dstImg = imaging.New(width, height, color.NRGBA{R: 255, G: 255, B: 255, A: 255})
+			dstImg = imaging.PasteCenter(dstImg, imaging.Fit(srcImg, width, height, imaging.Lanczos))
+		}
+
 		w.Header().Set("Content-Type", "image/jpg")
-		if err := imaging.Encode(w, img, imaging.JPEG); err != nil {
+		if err := imaging.Encode(w, dstImg, imaging.JPEG); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
